f60960eb4d4e30c46d44364c47887fe76d1441bd
Angelo Amadora <amadora.angelo@gmail.com>
2020-11-04 20:12:16 +0800

    removed flake for a while

--- app/core/models.py
+++ app/core/models.py
@@ -1,13 +1,26 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+PermissionsMixin
 
 # Create your models here.
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         #Create and saves a user
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('Users must have email address')
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        # creates a superuser
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -14,3 +14,25 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        #test if email becomes normalized
+        email = 'test@ANGELO.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        # test if user with no email raises an error
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'askjfdh')
+
+    def test_create_new_superuser(self):
+        # Test creating a super user
+        user = get_user_model().objects.create_superuser(
+            'test@angelo.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
