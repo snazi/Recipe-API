49fce01b94378cd928085293a9521c58d76ecf24
Angelo Amadora <amadora.angelo@gmail.com>
2020-11-23 19:18:30 +0800

    created recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -67,3 +67,17 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """
+        Test the recipe string representation
+        """
+
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Champorado',
+            time_minutes=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
